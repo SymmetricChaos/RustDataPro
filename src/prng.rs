269c0b67e3085ee34dba03a2c6f16c70af9e2784
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-25 12:14:07 -0400

    shuffle and random

--- src/prng.rs
+++ src/prng.rs
@@ -11,4 +11,11 @@ impl Prng {
         self.state = (self.state).wrapping_mul(1664525).wrapping_add(1013904223);
         (self.state ^ (self.state >> 22)) >> (22 + (self.state >> 61))
     }
+
+    pub fn shuffle<T>(&mut self, v: &mut Vec<T>) {
+        for i in 0..v.len() {
+            let swap_pos = self.next_u64() as usize % v.len();
+            v.swap(i, swap_pos);
+        }
+    }
 }
--- src/randomness_page.rs
+++ src/randomness_page.rs
@@ -1,24 +1,24 @@
 use crate::prng::Prng;
 use chrono::Local;
-use egui::{CentralPanel, Context, Slider, TextEdit};
+use egui::{CentralPanel, Context, DragValue, TextEdit};
 use itertools::Itertools;
 
 pub struct RandomnessPage {
     prng: Prng,
-    text: String,
     min_rand: usize,
     max_rand: usize,
     random_nums: String,
+    shuffle_list: String,
 }
 
 impl Default for RandomnessPage {
     fn default() -> Self {
         Self {
-            prng: Prng::new(Local::now().timestamp() as u64), // TODO: seed from system time
-            text: String::new(),
+            prng: Prng::new(Local::now().timestamp_micros() as u64),
             min_rand: 1,
             max_rand: 5,
             random_nums: String::new(),
+            shuffle_list: String::new(),
         }
     }
 }
@@ -26,47 +26,70 @@ impl Default for RandomnessPage {
 impl RandomnessPage {
     pub fn view(&mut self, ctx: &Context) {
         CentralPanel::default().show(ctx, |ui| {
-            //     ui.add(
-            //         TextEdit::multiline(&mut self.text)
-            //             .desired_width(800.0)
-            //             .desired_rows(10),
-            //     );
-            //     if ui.button("📋").clicked() {
-            //         ui.ctx().copy_text(self.text.to_string());
-            //     };
+            ui.heading("Random Numbers in a Range");
+            ui.horizontal(|ui| {
+                ui.add_space(2.0);
+                ui.label("min");
+                if ui
+                    .add(DragValue::new(&mut self.min_rand).range(0..=99))
+                    .changed()
+                {
+                    if self.min_rand >= self.max_rand {
+                        self.max_rand = self.min_rand + 1;
+                    }
+                };
+                ui.add_space(2.0);
+                ui.label("max");
+                if ui
+                    .add(DragValue::new(&mut self.max_rand).range(1..=100))
+                    .changed()
+                {
+                    if self.min_rand >= self.max_rand {
+                        self.min_rand = self.max_rand - 1;
+                    }
+                };
+            });
 
-            ui.label(format!(
-                "Random Numbers in the Range {} to {}",
-                self.min_rand, self.max_rand
-            ));
-            if ui.add(Slider::new(&mut self.min_rand, 0..=19)).changed() {
-                if self.min_rand >= self.max_rand {
-                    self.max_rand = self.min_rand + 1;
-                }
-            };
-            if ui.add(Slider::new(&mut self.max_rand, 1..=20)).changed() {
-                if self.min_rand >= self.max_rand {
-                    self.min_rand = self.max_rand - 1;
-                }
-            };
             if ui.button("Generate").clicked() {
-                let range = self.max_rand - self.min_rand + 1;
                 let mut v = Vec::new();
                 for i in self.min_rand..=self.max_rand {
                     v.push(i);
                 }
 
-                for i in 0..range {
-                    let swap_pos = self.prng.next_u64() as usize % range;
-                    v.swap(i, swap_pos);
+                self.prng.shuffle(&mut v);
+
+                let list = v.iter().map(|n| n.to_string()).join(", ");
+
+                if !self.random_nums.is_empty() {
+                    self.random_nums.push('\n');
                 }
 
-                self.random_nums = v.iter().map(|n| n.to_string()).join(", ");
+                self.random_nums.push_str(&list);
             }
             ui.add(
                 TextEdit::multiline(&mut self.random_nums)
                     .desired_width(400.0)
                     .desired_rows(2),
+            );
+
+            ui.add_space(10.0);
+            ui.heading("Shuffle a List");
+            ui.label("Separate items with commas.");
+            if ui.button("Shuffle").clicked() {
+                let substrs = self.shuffle_list.split(',');
+                let mut list: Vec<&str> = substrs.collect();
+                self.prng.shuffle(&mut list);
+                let rep = list
+                    .iter()
+                    .map(|s| s.trim())
+                    .filter(|s| s.len() > 0)
+                    .join(", ");
+                self.shuffle_list = rep;
+            }
+            ui.add(
+                TextEdit::multiline(&mut self.shuffle_list)
+                    .desired_width(400.0)
+                    .desired_rows(8),
             );
         });
     }
