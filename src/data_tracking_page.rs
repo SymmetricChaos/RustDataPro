bdda6b08876ff6fbb5cea38202d6a86a1c711958
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2026-06-29 12:29:51 -0400

    save data capability

--- src/data_tracking_page.rs
+++ src/data_tracking_page.rs
@@ -1,6 +1,7 @@
 use crate::ksf::Ksf;
 use chrono::{DateTime, Duration, Local};
 use egui::Ui;
+use egui_file_dialog::FileDialog;
 
 const MAX_DUR: usize = 20;
 const MAX_FREQ: usize = 20;
@@ -11,16 +12,22 @@ pub struct Session {
     total_times: [Duration; MAX_DUR],
     timers_active: [bool; MAX_DUR],
     counters: [u32; MAX_FREQ],
+    session_start_time: DateTime<Local>,
+    file_dialog: FileDialog,
+    output_file_contents: String
 }
 
 impl Default for Session {
     fn default() -> Self {
         Self {
             ksf: Ksf::new(),
+            session_start_time: Local::now(),
             init_times: [Local::now(); MAX_DUR],
             total_times: [Duration::zero(); MAX_DUR],
             timers_active: [false; MAX_DUR],
             counters: [0; MAX_FREQ],
+            file_dialog: FileDialog::new().default_file_name("SaveData.txt"),
+            output_file_contents: String::new(),
         }
     }
 }
@@ -31,6 +38,55 @@ impl Session {
     }
 
     pub fn view(&mut self, ui: &mut Ui) {
+        egui::Panel::top("data_top_panel").show(ui, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
+
+
+                if ui.button("Start Session").clicked() {
+                    self.session_start_time = Local::now();
+                }
+
+                if ui.button("End Session").clicked() {
+
+
+                    self.output_file_contents.clear();
+
+
+                    // Save duration data
+                    for (idx, keybind) in self.ksf.duration.iter().enumerate() {
+                        // Stop timers if they are running and update the total
+                        if self.timers_active[idx] {
+                            self.total_times[idx] += Local::now() - self.init_times[idx];
+                            self.timers_active[idx] = false;
+                        }
+                        self.output_file_contents.push_str(&format!("{} {}\n",keybind.description, self.total_times[idx].as_seconds_f32()));
+                    };
+                    // Save frequency data
+                    for (idx, keybind) in self.ksf.frequency.iter().enumerate() {
+                        self.output_file_contents.push_str(&format!("{} {}\n",keybind.description, self.counters[idx]));
+                    };
+
+                    self.init_times = [Local::now(); MAX_DUR];
+                    self.total_times = [Duration::zero(); MAX_DUR];
+                    self.timers_active = [false; MAX_DUR];
+                    self.counters = [0; MAX_FREQ];
+
+                    // Open save dialog
+                    self.file_dialog.save_file();
+
+                }
+                
+                self.file_dialog.update(ui.ctx());
+
+                if let Some(path) = self.file_dialog.take_picked() {
+                    if std::fs::write(&path, &self.output_file_contents).is_ok() {
+                        println!("Successfully saved to: {:?}", path);
+                    }
+                }
+
+            });
+        });
+
         egui::CentralPanel::default().show(ui, |ui| {
             ui.heading("Timers");
 
